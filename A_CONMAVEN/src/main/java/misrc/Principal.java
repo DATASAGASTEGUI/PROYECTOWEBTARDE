322b6eea1906cdc324a38b13c072cb2ac35bb946
Walter Ismael Sagástegui Lescano <datasagamadrid@gmail.com>
2022-11-23 15:53:18 +0100

    TITULO

---
+++ A_CONMAVEN/src/main/java/misrc/Principal.java
@@ -0,0 +1,14 @@
+package misrc;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        System.out.println("HOLA MUNDO");
+        
+        
+        
+       
+    }
+
+}
