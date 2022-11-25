495ab3100b81c00def60918bb80c35bd528286f9
Walter Ismael Sagástegui Lescano <datasagamadrid@gmail.com>
2022-11-25 19:03:10 +0100

    TITULO

--- MONGOWEB/src/main/java/misrc/OperacionesCrud.java
+++ MONGOWEB/src/main/java/misrc/OperacionesCrud.java
@@ -4,41 +4,87 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+
+import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.bson.Document;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class OperacionesCrud {
 
-    private MongoDatabase conexion;
+    private MongoDatabase conexion_mo;
+    private Connection conexion_my;
 
-    public OperacionesCrud() {
-        MongoClient cliente = new MongoClient("localhost", 27017);
-        conexion = cliente.getDatabase("colegio");
+    public OperacionesCrud(int x) {//1: MONGODB  2:MYSQL
+
+        if (x == 1) {
+            MongoClient cliente = new MongoClient("localhost", 27017);
+            conexion_mo = cliente.getDatabase("colegio");
+        }
+
+        if (x == 2) {
+            String url = "jdbc:mysql://localhost:3306/colegio";  //protocolo-IP-PUERTO-BD
+            String usuario = "root";
+            String clave = "12345678";
+            try {
+                Class.forName("com.mysql.jdbc.Driver");//Registrar el driver
+                conexion_my = DriverManager.getConnection(url, usuario, clave);
+            } catch (Exception e) {
+                conexion_my = null;
+            }
+        }
     }
 
-    public Map<String, Alumno> getHashMapAlumno() {
+    public Map<String, Alumno> getHashMapAlumno(int x) { //1: MONGODB  2:MYSQL
         Map<String, Alumno> alumnos_hm = new HashMap<>();
-        try {
-            MongoCollection<Document> coleccion = conexion.getCollection("alumno");
-            MongoCursor<Document> cursor = coleccion.find().iterator();
-            while (cursor.hasNext()) {
-                Document documento = cursor.next();
-                List lista = new ArrayList(documento.values());
-                String idAlumno = (String) lista.get(1);
-                String nombre = (String) lista.get(2);
-                int edad = (int) lista.get(3);
-                double estatura = (double) lista.get(4);
-                String foto = (String) lista.get(5);
 
-                Alumno alumno = new Alumno(idAlumno, nombre, edad, estatura, foto);
-                alumnos_hm.put(idAlumno, alumno);
+        if (x == 1) {
+            try {
+                MongoCollection<Document> coleccion = conexion_mo.getCollection("alumno");
+                MongoCursor<Document> cursor = coleccion.find().iterator();
+                while (cursor.hasNext()) {
+                    Document documento = cursor.next();
+                    List lista = new ArrayList(documento.values());
+                    String idAlumno = (String) lista.get(1);
+                    String nombre = (String) lista.get(2);
+                    int edad = (int) lista.get(3);
+                    double estatura = (double) lista.get(4);
+                    String foto = (String) lista.get(5);
+
+                    Alumno alumno = new Alumno(idAlumno, nombre, edad, estatura, foto);
+                    alumnos_hm.put(idAlumno, alumno);
+                }
+            } catch (Exception e) {
+                alumnos_hm = null;
             }
-        } catch (Exception e) {
-            alumnos_hm = null;
         }
+
+        if (x == 2) {
+            String query = "SELECT * FROM Alumno";
+            try {
+                PreparedStatement ps = conexion_my.prepareStatement(query);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    String idAlumno = rs.getString(1);
+                    String nombre = rs.getString(2);
+                    int edad = rs.getInt(3);
+                    double estatura = rs.getDouble(4);
+                    String foto = rs.getString(5);
+                    Alumno alumno = new Alumno(idAlumno, nombre, edad, estatura, foto);
+                    alumnos_hm.put(alumno.getIdAlumno(), alumno);
+                }
+            } catch (Exception e) {
+                alumnos_hm = null;
+            }
+
+        }
+
         return alumnos_hm;
     }
 }
--- MONGOWEB/src/main/webapp/mostrar.jsp
+++ MONGOWEB/src/main/webapp/mostrar.jsp
@@ -4,6 +4,10 @@
     Author     : RYZEN
 --%>
 
+<%@page import="java.util.Collection"%>
+<%@page import="java.util.Map"%>
+<%@page import="misrc.Alumno"%>
+<%@page import="misrc.OperacionesCrud"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -12,6 +16,24 @@
         <title>MOSTRAR</title>
     </head>
     <body>
-        <h1>MOSTRAR</h1>
-    </body>
+    <center><h1>MOSTRAR</h1></center>
+        <%
+            OperacionesCrud crud = new OperacionesCrud(2);
+            Map<String, Alumno> alumnos_hm = crud.getHashMapAlumno(2);
+            Collection coleccion = alumnos_hm.values();
+            for (Object o : coleccion) {
+                Alumno a = (Alumno) o;
+                String ruta = request.getContextPath() + "/image/" + a.getFoto();
+        %>       
+    <center>
+        <input type="text" value='<%=a.getIdAlumno()%>' />
+        <input type="text" value='<%=a.getNombre()%>' />
+        <input type="text" value='<%=a.getEdad()%>' />
+        <input type="text" value='<%=a.getEstatura()%>' />
+        <img src='<%=ruta%>' />
+    </center>
+    <%
+        }
+    %>
+</body>
 </html>
